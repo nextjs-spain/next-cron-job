d56557eee07deaf4ee86353694925c7c76c8735a
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-10-16 22:01:53 +0200

    sc

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
-## Getting Started
-
-First, run the development server:
-
-```bash
 /** @type {import('next').NextConfig} */
 const cron = require('node-cron');
 
-cron.schedule('* * * * *', function () {
+cron.schedule('* * * * * *', function () {
   console.log('Say scheduled hello')
 });
 
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
+
 module.exports = nextConfig
-```
